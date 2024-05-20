65e2f41662de475caa84bd816e16b92136b787ef
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-20 18:13:03 +0200

    Added js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('funguju!');
+const alarm = document.querySelector('.alarm');
+const audio = document.querySelector('audio');
+const button = document.querySelector('button');
+const seconds = Number(prompt('Zadejte počet sekund k odpočtu:'));
+const ring = () => {
+  alarm.classList.add('alarm--ring');
+  audio.play();
+};
+const timeout = setTimeout(ring, seconds * 1000);
+button.addEventListener('click', () => {
+  clearTimeout(timeout);
+});
